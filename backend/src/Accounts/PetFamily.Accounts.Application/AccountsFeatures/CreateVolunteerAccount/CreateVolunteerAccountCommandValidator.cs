e2cc02446f21b1ec0142c888494960ff6371654b
cl1nt0n <igorchinakov@mail.ru>
2025-07-21 12:57:03 +0300

    add accounts features

---
+++ backend/src/Accounts/PetFamily.Accounts.Application/AccountsFeatures/CreateVolunteerAccount/CreateVolunteerAccountCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using PetFamily.Core.Extensions;
+using PetFamily.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFamily.Accounts.Application.AccountsFeatures.CreateVolunteerAccount
+{
+    public class CreateVolunteerAccountCommandValidator
+        : AbstractValidator<CreateVolunteerAccountCommand>
+    {
+        public CreateVolunteerAccountCommandValidator()
+        {
+            RuleFor(c => c.UserId)
+                .NotEmpty()
+                .WithError(Errors.General.ValueIsInvalid("Userid"));
+        }
+    }
+}
--- backend/src/Accounts/PetFamily.Accounts.Domain/ValueObjects/FullName.cs
+++ backend/src/Accounts/PetFamily.Accounts.Domain/ValueObjects/FullName.cs
@@ -10,7 +10,7 @@ namespace PetFamily.Accounts.Domain.ValueObjects
     {
         public string Name { get; set; } = string.Empty;
 
-        public string SecondName { get; set; } = string.Empty;
+        public string? SecondName { get; set; } = string.Empty;
 
         public string FamilyName { get; set; } = string.Empty;
     }
